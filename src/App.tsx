43d7c9ed3e1ea972735716700d003b9c886e5e29
Sean <seand52@gmail.com>
2019-11-25 17:03:47 +0100

    Install material UI and css setup
    Modify tsconfig to import absolute paths from src/
    
    Remove test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import './App.css';
+import 'App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
-  );
+  return <div className="App"></div>;
 };
 
 export default App;
